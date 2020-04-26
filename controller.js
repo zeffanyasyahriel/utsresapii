0885e9d4b3176e21be7fdc15b096a23f4e6c1cf0
zeffanyasyahriel <zeffanyasyahriel244@outlook.com>
2020-04-26 15:34:13 +0700

    berhasil menampilkan data sparepart dan montir berdasarkan id

--- controller.js
+++ controller.js
@@ -27,3 +27,27 @@ exports.tampildatamontir = function (req, res) {
 		}
 	});
 };
+//menampilkan data sparepart berdasarkan id
+exports.tampildatasparepartid = function (req, res) {
+	let id = req.params.id;
+	connection.query('SELECT * FROM t_sparepart WHERE id_sparepart = ?', [id],
+		function (error, rows, fields) {
+			if (error) {
+				console.log(error);
+			} else {
+				response.ok(rows, res);
+			}
+		});
+};
+//menampilkan data montir berdasarkan id
+exports.tampildatamontirid = function (req, res) {
+	let id = req.params.id;
+	connection.query('SELECT * FROM t_montir WHERE id_montir = ?', [id],
+		function (error, rows, fields) {
+			if (error) {
+				console.log(error);
+			} else {
+				response.ok(rows, res);
+			}
+		});
+};
--- routes.js
+++ routes.js
@@ -10,4 +10,10 @@ module.exports = function(app) {
 
          app.route('/tampilmontir')
          .get(jsonku.tampildatamontir);
+
+         app.route('/tampil/:id')
+         .get(jsonku.tampildatasparepartid);
+         
+         app.route('/tampilmontir/:id')
+            .get(jsonku.tampildatamontirid);
 }
